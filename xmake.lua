dc13474b0abd3e0820158d1d004cb2f42eb5eb1e
phoohow <phoohow@126.com>
2025-08-03 10:15:02 +0800

    update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,43 +1,34 @@
 set_languages("c++20")
+add_rules("mode.debug", "mode.release")
 
-target("ShaderBlob")
+target("ShaderBlob", function()
     set_kind("shared")
-    if get_config("buildir")~=nil then 
-        set_targetdir(get_config("buildir").."/bin")
-    end
-    add_includedirs("include", {
-        public = true
-    })
+    add_includedirs("include", {public = true})
     add_files("src/ShaderBlob.cpp")
+    
     if is_plat("windows") then
         add_defines("SHADERBLOB_EXPORT_DLL")
     end
-target_end()
+end)
 
-target("ShaderMake")
+target("ShaderMake", function()
     set_kind("binary")
-    if get_config("buildir")~=nil then 
-        set_targetdir(get_config("buildir").."/bin")
-    end
     add_files("src/argparse.c", "src/ShaderMake.cpp")
     add_deps("ShaderBlob")
+    
     if is_plat("windows") then
         add_links("d3dcompiler", "dxcompiler", "delayimp")
     end
-target_end()
+end)
 
-target("ShaderMakeLib")
+target("ShaderMakeLib", function()
     set_kind("shared")
-    if get_config("buildir")~=nil then 
-        set_targetdir(get_config("buildir").."/bin")
-    end
-    add_includedirs("include", {
-        public = true
-    })
+    add_includedirs("include", {public = true})
     add_files("src/argparse.c", "src/ShaderMakeLib.cpp")
     add_deps("ShaderBlob")
+    
     if is_plat("windows") then
         add_defines("SHADERMAKE_EXPORT_DLL")
         add_links("d3dcompiler", "dxcompiler", "delayimp")
     end
-target_end()
+end)
